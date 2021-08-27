3504d842d887ba89e7275cb9fcad62ca0627c9e9
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2021-08-27 13:06:42 -0500

    ADD initial Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { fetcher } from "./utils";
+import useSWR from "swr";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const { data, error } = useSWR(
+    "https://cdn.contentful.com/spaces/9ugiyt41p6d2/environments/master/entries?access_token=66dJAhJ0t5bD3sAHpY3GE-SEenRaOWikMeBjts2FlV4",
+    fetcher
   );
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+
+  // render data
+  return <div>hay {data.total} blogs !</div>;
 }
 
 export default App;
