19a1a3016eef08a5998c3277576a468311678541
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2021-08-29 18:36:24 -0500

    ADD react markdown description

--- src/App.js
+++ src/App.js
@@ -1,30 +1,71 @@
 import * as React from "react";
 import "./App.css";
 import { fetcher } from "./utils";
-import useSWR from "swr";
-
 import ReactMarkdown from "react-markdown";
+import useSWR from "swr";
+import { useParams } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
+function Home() {
   const { data, error } = useSWR(
     "https://cdn.contentful.com/spaces/9ugiyt41p6d2/environments/master/entries?access_token=66dJAhJ0t5bD3sAHpY3GE-SEenRaOWikMeBjts2FlV4",
     fetcher
   );
 
-  if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
+  if (error) return <div>Ha ocurrido un error compadre</div>;
+  if (!data) return <div>Estamos cargando mi KING</div>;
 
-  // render data
   return (
     <div>
       <h1>hay {data.total} blogs !</h1>
-      {/* React.Children.ToArray asigna una key automatica */}
-      {React.Children.toArray(
-        data.items.map((item) => <a href="">{item.fields.title}</a>)
-      )}
-      <ReactMarkdown># Hello, *world*!</ReactMarkdown>
+      <div className="lista-deBlogs">
+        {React.Children.toArray(
+          data.items.map((item) => (
+            <Link to={`/blog/${item.sys.id}`}>{item.fields.title}</Link>
+          ))
+        )}
+      </div>
     </div>
   );
 }
 
-export default App;
+function Blog() {
+  const params = useParams();
+  console.log(params);
+  const { data, error } = useSWR(
+    `https://cdn.contentful.com/spaces/9ugiyt41p6d2/environments/master/entries/${params.id}?access_token=66dJAhJ0t5bD3sAHpY3GE-SEenRaOWikMeBjts2FlV4`,
+    fetcher
+  );
+
+  if (error) return <div>Ha ocurrido un error compadre</div>;
+  if (!data) return <div>Estamos cargando mi KING</div>;
+
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+      </nav>
+      <h1>Featured Blogs</h1>
+      <ReactMarkdown>{data.fields.description}</ReactMarkdown>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/blog/:id">
+          <Blog />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
